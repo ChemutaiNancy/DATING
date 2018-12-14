32f6390bee5cdd6fc4883d192c20f10ea7aa5cf0
Nancy <chemutain@gmail.com>
2018-12-14 15:34:21 +0300

    dating app

---
+++ app/src/main/java/com/nancy/tinder/Matches/MatchesActivity.java
@@ -0,0 +1,114 @@
+package com.nancy.tinder.Matches;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.nancy.tinder.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MatchesActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mMatchesAdapter;
+    private RecyclerView.LayoutManager mMatchesLayoutManager;
+
+    private String cusrrentUserID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matches);
+        Toast.makeText(this, "Fetching", Toast.LENGTH_SHORT).show();
+
+        cusrrentUserID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        mRecyclerView.setNestedScrollingEnabled(false);
+        mRecyclerView.setHasFixedSize(true);
+        mMatchesLayoutManager = new LinearLayoutManager(MatchesActivity.this);
+        mRecyclerView.setLayoutManager(mMatchesLayoutManager);
+        mMatchesAdapter = new MatchesAdapter(getDataSetMatches(), MatchesActivity.this);
+        mRecyclerView.setAdapter(mMatchesAdapter);
+
+        getUserMatchId();
+        Log.d(TAG, "onCreate: Fetching matches");
+
+
+    }
+
+    String TAG = "Matches";
+    private void getUserMatchId() {
+
+        DatabaseReference matchDb = FirebaseDatabase.getInstance().getReference().child("Users").child(cusrrentUserID).child("connections").child("matches");
+        matchDb.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    Toast.makeText(MatchesActivity.this, "Existing", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "onDataChange: "+dataSnapshot.toString());
+                    for(DataSnapshot match : dataSnapshot.getChildren()){
+                        FetchMatchInformation(match.getKey());
+                    }
+                }
+                else{
+                    Toast.makeText(MatchesActivity.this, "Not existing", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "onDataChange: snapshot does not exist");
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void FetchMatchInformation(String key) {
+        DatabaseReference userDb = FirebaseDatabase.getInstance().getReference().child("Users").child(key);
+        userDb.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    String userId = dataSnapshot.getKey();
+                    String name = "";
+                    String profileImageUrl = "";
+                    if(dataSnapshot.child("name").getValue()!=null){
+                        name = dataSnapshot.child("name").getValue().toString();
+                    }
+                    if(dataSnapshot.child("profileImageUrl").getValue()!=null){
+                        profileImageUrl = dataSnapshot.child("profileImageUrl").getValue().toString();
+                    }
+
+
+                    MatchesObject obj = new MatchesObject(userId, name, profileImageUrl);
+                    resultsMatches.add(obj);
+                    mMatchesAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private ArrayList<MatchesObject> resultsMatches = new ArrayList<MatchesObject>();
+    private List<MatchesObject> getDataSetMatches() {
+        return resultsMatches;
+    }
+
+}
